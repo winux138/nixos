2e6e6008e20a78bd04dfb910ddae385f111a7ed8
ju <thomas.julien@protonmail.com>
2025-07-22 09:45:08 +0200

    Mirror from /etc/nixos

---
+++ home.nix
@@ -0,0 +1,25 @@
+{config, pkgs, ...}:
+
+{
+  home.username = "ju";
+  home.homeDirectory = "/home/ju";
+  home.stateVersion = "25.05";
+
+  programs.foot = {
+    enable = true;
+    settings = {
+      main = {
+        font = "Iosevka:size=16";
+        dpi-aware = "yes";
+      };
+
+      security = {
+        osc52 = "enabled";
+      };
+
+      mouse = {
+        hide-when-typing = "yes";
+      };
+    };
+  };
+}
