b12249657f1a0d008bc57a55292b738d66227b54
ju <thomas.julien@protonmail.com>
2025-07-22 15:24:53 +0200

    Use flake from "vimjoyer" template

--- configuration.nix
+++ configuration.nix
@@ -4,9 +4,6 @@
 
 { config, pkgs, ... }:
 
-let 
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-25.05.tar.gz";
-in
 {
   imports =
     [ # Include the results of the hardware scan.
