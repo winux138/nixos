fed137216aaff46f6ac57ca1c355f9e169423dfd
Julien THOMAS <thomas.julien@protonmail.com>
2025-07-22 18:22:03 +0200

    Fix flake + remove hardware config

--- configuration.nix
+++ configuration.nix
@@ -18,17 +18,18 @@
 
   # Bootloader.
   boot.loader = {
+    systemd-boot.enable = true;
     efi = {
       canTouchEfiVariables = true;
       # efiSysMountPoint = "/boot/efi"; # ← use the same mount point here.
     };
-    grub = {
-       enable = true;
-       efiSupport = true;
-       #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-       device = "nodev";
-       useOSProber = true;
-    };
+    # grub = {
+    #    enable = true;
+    #    efiSupport = true;
+    #    #efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+    #    device = "nodev";
+    #    useOSProber = true;
+    # };
   };
 
   # Use latest kernel.
@@ -43,38 +44,38 @@
   hardware.bluetooth.enable = true;
 
   # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
+  # services.xserver.videoDrivers = ["nvidia"];
 
-  hardware.nvidia = {
+  # hardware.nvidia = {
 
-    # Modesetting is required.
-    modesetting.enable = true;
+  #   # Modesetting is required.
+  #   modesetting.enable = true;
 
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
+  #   # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+  #   # Enable this if you have graphical corruption issues or application crashes after waking
+  #   # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
+  #   # of just the bare essentials.
+  #   powerManagement.enable = false;
 
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
+  #   # Fine-grained power management. Turns off GPU when not in use.
+  #   # Experimental and only works on modern Nvidia GPUs (Turing or newer).
+  #   powerManagement.finegrained = false;
 
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    open = false;
+  #   # Use the NVidia open source kernel module (not to be confused with the
+  #   # independent third-party "nouveau" open source driver).
+  #   # Support is limited to the Turing and later architectures. Full list of 
+  #   # supported GPUs is at: 
+  #   # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+  #   # Only available from driver 515.43.04+
+  #   open = false;
 
-    # Enable the Nvidia settings menu,
-	# accessible via `nvidia-settings`.
-    nvidiaSettings = true;
+  #   # Enable the Nvidia settings menu,
+  #       # accessible via `nvidia-settings`.
+  #   nvidiaSettings = true;
 
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
+  #   # Optionally, you may need to select the appropriate driver version for your specific GPU.
+  #   package = config.boot.kernelPackages.nvidiaPackages.stable;
+  # };
 
 
 
@@ -169,21 +170,27 @@
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  wayland.windowManager.hyprland = {
-    enable = true;
-    withUWSM = true; # recommended for most users
-    xwayland.enable = true; # Xwayland can be disabled.
-
-    plugins = [
-      inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo
-    ];
-  };
-
-  # programs.hyprland = {
+  # wayland.windowManager.hyprland = {
   #   enable = true;
   #   withUWSM = true; # recommended for most users
   #   xwayland.enable = true; # Xwayland can be disabled.
+  #   environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  #   plugins = [
+  #     inputs.hyprland-plugins.packages.${pkgs.stdenv.hostPlatform.system}.hyprexpo
+  #   ];
   # };
+
+  programs.hyprland = {
+    enable = true;
+    withUWSM = true; # recommended for most users
+    xwayland.enable = true; # Xwayland can be disabled.
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+    # make sure to also set the portal package, so that they are in sync
+    portalPackage = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.xdg-desktop-portal-hyprland;
+  };
+
+    environment.sessionVariables.NIXOS_OZONE_WL = "1";
+    systemd.services.nix-daemon.environment.TMPDIR = "/var/tmp";
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
--- flake.nix
+++ flake.nix
@@ -29,7 +29,7 @@
   } @ inputs: {
     # use "nixos", or your hostname as the name of the configuration
     # it's a better practice than "default" shown in the video
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.ju = nixpkgs.lib.nixosSystem {
       specialArgs = {inherit inputs;};
       modules = [
         ./configuration.nix
