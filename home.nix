6e3ab9d7394a3bbc6b840ee53a662cbfc1830aa7
ju <thomas.julien@protonmail.com>
2025-07-25 11:45:24 +0200

    git: Add initial config

--- home.nix
+++ home.nix
@@ -313,6 +313,45 @@
   xdg.mime.enable = true;
   xdg.mimeApps.enable = true;
 
+  programs.git = {
+    enable = true;
+    lfs.enable = true;
+    extraConfig = {
+      branch.sort = "-committerdate";
+      column.ui = "auto";
+      commit.verbose = true;
+      diff = {
+        algorithm = "histogram";
+        colorMoved = "plain";
+        mnemonicPrefix = true;
+        renames = true;
+      };
+      fetch = {
+        all = true;
+        prune = true;
+        pruneTags = true;
+      };
+      init.defaultBranch = "main";
+      merge.conflictStyle = "zdiff3";
+      pull.rebase = true;
+      push = {
+        autoSetupRemote = true;
+        default = "simple";
+        followTags = true;
+      };
+      rebase = {
+        autoSquash = true;
+        autoStash = true;
+        updateRefs = true;
+      };
+      rerere = {
+        autoUpdate = true;
+        enabled = true;
+      };
+      tag.sort = "version:refname";
+    };
+  };
+
   programs.foot = {
     enable = true;
     settings = {
