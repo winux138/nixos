0f289e6c26caf260f578c4f70d51afcafc32081c
Julien THOMAS <thomas.julien@protonmail.com>
2026-03-21 00:31:55 +0100

    wip

--- configuration.nix
+++ configuration.nix
@@ -32,6 +32,12 @@
     # };
   };
 
+  boot.binfmt.emulatedSystems = [
+    "aarch64-linux"
+  ];
+  # list of (one of , "aarch64_be-linux", "alpha-linux", "armv6l-linux", "armv7l-linux", "i386-linux", "i486-linux", "i586-linux", "i686-linux", "i686-windows", "loongarch64-linux", "mips-linux", "mips64-linux", "mips64-linuxabin32", "mips64el-linux", "mips64el-linuxabin32", "mipsel-linux", "powerpc-linux", "powerpc64-linux", "powerpc64le-linux", "riscv32-linux", "riscv64-linux", "s390x-linux", "sparc-linux", "sparc64-linux", "wasm32-wasi", "wasm64-wasi", "x86_64-linux", "x86_64-windows")
+  nix.settings.extra-platforms = config.boot.binfmt.emulatedSystems;
+
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
@@ -67,6 +73,7 @@
   }; # Force intel-media-driver
 
   hardware.bluetooth.enable = true;
+  hardware.opentabletdriver.enable = true;
 
   # # Load nvidia driver for Xorg and Wayland
   # services.xserver.videoDrivers = ["nvidia"];
@@ -132,6 +139,7 @@
     layout = "us";
     variant = "";
   };
+  services.xserver.wacom.enable = true;
 
   services.gvfs.enable = true;
   services.udisks2.enable = true;
@@ -161,7 +169,10 @@
   services.upower.enable = true;
   services.safeeyes.enable = true;
 
-  environment.pathsToLink = ["/share/applications" "/share/xdg-desktop-portal"];
+  environment.pathsToLink = [
+    "/share/applications"
+    "/share/xdg-desktop-portal"
+  ];
 
   # nixpkgs.overlays = [
   #   (import (
@@ -239,7 +250,7 @@
   # programs.gamemode.enable = true;
 
   programs.virt-manager.enable = true;
-  users.groups.libvirtd.members = ["ju"];
+  users.groups.libvirtd.members = [ "ju" ];
   virtualisation.libvirtd.enable = true;
   virtualisation.spiceUSBRedirection.enable = true;
   # virtualisation.libvirtd.hooks.network = ''
@@ -294,6 +305,9 @@
     ungoogled-chromium
     qutebrowser
     calibre
+    krita
+    kdePackages.wacomtablet
+    opentabletdriver
   ];
 
   programs.nix-ld.enable = true;
@@ -336,7 +350,7 @@
     settings.PasswordAuthentication = true;
   };
   systemd.services.sshd = {
-    wantedBy = lib.mkForce [];
+    wantedBy = lib.mkForce [ ];
     stopIfChanged = lib.mkForce true;
   };
 
